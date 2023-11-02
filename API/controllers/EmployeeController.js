15f55b3017c6c7c1e427a10958703415b4d12c13
Evan Naderi <evan.naderi@gmail.com>
2023-11-02 10:50:53 -0400

    refined register and added password hashing

--- API/controllers/EmployeeController.js
+++ API/controllers/EmployeeController.js
@@ -1,22 +1,27 @@
 const Employee = require('../models/Employee');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // for bcrypt password hashing
 
-Employee.syncIndexes().then(() => {
-  console.log('Indexes have been synchronized');
-}).catch(err => {
-  console.log('Error synchronizing indexes:', err);
-});
+// Was used for unique index testing
 
-Employee.collection.getIndexes({ full: true }).then(indexes => {
-  console.log("indexes:", indexes);
-}).catch(console.error);
+// Employee.syncIndexes().then(() => {
+//   console.log('Indexes have been synchronized');
+// }).catch(err => {
+//   console.log('Error synchronizing indexes:', err);
+// });
 
-Employee.collection.dropIndex('_id_', function(err, result) {
-  if (err) {
-    console.log('Error in dropping index!', err);
-  } else {
-    console.log('Index dropped:', result);
-  }
-});
+// Employee.collection.getIndexes({ full: true }).then(indexes => {
+//   console.log("indexes:", indexes);
+// }).catch(console.error);
+
+// Employee.collection.dropIndex('_id_', function(err, result) {
+//   if (err) {
+//     console.log('Error in dropping index!', err);
+//   } else {
+//     console.log('Index dropped:', result);
+//   }
+//});
 
 exports.registerEmployee = async (req, res) => {
   try {
@@ -27,14 +32,11 @@ exports.registerEmployee = async (req, res) => {
       firstName,
       lastName,
       phone,
-      empId,
-      positionId,
-      title,
-      dayAvail,
-      endAvail,
-      startAvail,
       managerIdent,
-      managedBy
+      managedBy,
+      availability,
+      positions, // Assuming this is an array of position IDs
+      preference
     } = req.body;
 
     // Validations
@@ -46,30 +48,34 @@ exports.registerEmployee = async (req, res) => {
       return res.status(400).json({ message: 'Username must be between 2 to 20 characters' });
     }
 
-    // TODO: Hash password before storing it
+    // Hash password before storing it
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // Assuming that `positions` and `preference` arrays are passed in the request as needed
     const newEmployee = new Employee({
+      _id: new mongoose.Types.ObjectId(), // generate a new ObjectId
       username,
-      password, 
+      password: hashedPassword, // store the hashed password
       email,
       firstName,
       lastName,
       phone,
-      empId,
-      positionId,
-      title,
-      dayAvail,
-      endAvail,
-      startAvail,
       managerIdent,
-      managedBy
+      managedBy,
+      availability, // make sure to validate the structure on the client side or before saving
+      positions, // ensure this is an array of ObjectId references to the Position model
+      preference, // as above, validate structure before saving
+      __v: 0 // typically this is handled by Mongoose and does not need to be set manually
     });
 
     const savedEmployee = await newEmployee.save();
     console.log('Saved employee with ID:', savedEmployee._id);
-    const foundEmployee = await Employee.findById("652deb6f8f4b62b8b05b8743");
-    console.log('Directly queried employee:', foundEmployee);
+    
+    // Not sure why the foundEmployee is queried right after saving, seems redundant
+    // const foundEmployee = await Employee.findById(savedEmployee._id);
+    // console.log('Directly queried employee:', foundEmployee);
 
-    res.status(201).json({ message: 'Employee registered successfully', newEmployee });
+    res.status(201).json({ message: 'Employee registered successfully', employeeId: savedEmployee._id });
   } catch (err) {
     if (err.code === 11000) {
       return res.status(400).json({ message: 'Username already exists' });
@@ -81,10 +87,7 @@ exports.registerEmployee = async (req, res) => {
 
 exports.loginEmployee = async (req, res) => {
   try {
-    const {
-      username,
-      password
-    } = req.body
+    const { username, password } = req.body;
 
     if (!username || !password) {
       return res.status(400).json({ message: 'Please enter both a username and password' });
@@ -92,25 +95,22 @@ exports.loginEmployee = async (req, res) => {
 
     const user = await Employee.findOne({ username: { $regex: new RegExp(username, 'i') } });
 
-    console.log(user)
-    console.log(username)
-
-    // change error messages to be more vague later
-    // need to hash password (bcrypt?)
     if (!user) {
-      return res.status(401).json({ message: 'Invalid username' });
+      // It's generally a good security practice to give the same error message for both invalid username or password
+      return res.status(401).json({ message: 'Invalid username or password' });
     }
 
-    if (user.password !== password) {
-      return res.status(401).json({ message: 'Invalid password' });
+    // Compare provided password with the hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid username or password' });
     }
 
+    // If the password matches, proceed to login
     return res.json({ message: 'Login successful', id: user._id });
-  }
-
-  catch (error) {
+  } catch (error) {
     res.status(500).json({ message: 'Error authenticating user', error });
-    console.error("There was an error logging in", error );
+    console.error("There was an error logging in:", error);
   }
 };
 
