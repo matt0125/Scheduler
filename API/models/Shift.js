ff43ec27377fae80255286f6da879f3011c7fee8
zeorphix <tieue45@gmail.com>
2023-11-05 01:10:16 -0500

    updated Shift/ShiftTemplate schema

--- API/models/Shift.js
+++ API/models/Shift.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 // Define the schema for the 'Shift' collection
 const shiftSchema = new mongoose.Schema({
-  date: String,
-  empId: String,
-  templateId: String,
+  date: Date,
+  empId: Schema.Types.ObjectId,
+  templateId: Schema.Types.ObjectId,
 });
 
 // Create a model for the 'Shift' collection and export it
