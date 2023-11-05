cccde331477372022f6b2b99b40d65b3e24022a4
Evan Naderi <evan.naderi@gmail.com>
2023-11-05 09:06:37 -0500

    create shift validation

--- API/models/Employee.js
+++ API/models/Employee.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Position = require('./Position');
 
 // Availability subdocument schema
 const AvailabilitySchema = new Schema({
---
+++ API/models/Position.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// Define the schema for the 'Position' collection
+const positionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true // if 'name' is always required, otherwise remove this line
+  }
+});
+
+// Create a model for the 'Position' collection and export it
+module.exports = mongoose.model('Position', positionSchema, 'Position');
