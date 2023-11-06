5f144d3964d7a34a124cf66d4e0bd5327e8e6c83
Evan Naderi <evan.naderi@gmail.com>
2023-11-06 18:36:40 -0500

    create shift verifications

--- API/models/ShiftTemplate.js
+++ API/models/ShiftTemplate.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const shiftTemplateSchema = new mongoose.Schema({
-  dayOfWeek: String,
+  dayOfWeek: Number,
   startTime: String,
   endTime: String,
-  positionId: String,
+  positionId:  Schema.Types.ObjectId,
 });
 
 module.exports = mongoose.model('ShiftTemplate', shiftTemplateSchema, 'ShiftTemplate');
