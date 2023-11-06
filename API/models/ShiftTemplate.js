335810652420b052f26e9d7f4f9e073f001b138f
Evan <tieue45@gmail.com>
2023-11-06 18:52:19 -0500

    syntax fix

--- API/models/ShiftTemplate.js
+++ API/models/ShiftTemplate.js
@@ -5,11 +5,7 @@ const shiftTemplateSchema = new mongoose.Schema({
   dayOfWeek: Number,
   startTime: String,
   endTime: String,
-<<<<<<< HEAD
-  positionId:  Schema.Types.ObjectId,
-=======
   positionId: Schema.Types.ObjectId,
->>>>>>> 7ed8e9b42c1818af2e91f87b5ed041d67a2f5758
 });
 
 module.exports = mongoose.model('ShiftTemplate', shiftTemplateSchema, 'ShiftTemplate');
