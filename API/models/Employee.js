8b4b8809236276c6df739b1f9ee0e37d6088b46e
Matthew Daley <m.daley0125@gmail.com>
2023-11-07 14:37:39 -0500

    Update Employee.js

--- API/models/Employee.js
+++ API/models/Employee.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Position = require('./Position');
 
 // Availability subdocument schema
 const AvailabilitySchema = new Schema({
