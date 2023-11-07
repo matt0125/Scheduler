812162d2c04921af95b41ec1590392031e9e6b75
Firebro51 <140781290+Firebro51@users.noreply.github.com>
2023-11-07 14:40:14 -0500

    Got calendar to start at 9am and go to 5am
    Got calendar to start at 9am and go to 5am

--- web/src/pages/Dashboard.js
+++ web/src/pages/Dashboard.js
@@ -1,43 +1,27 @@
-import React from 'react';
-//Importing Calendar Modules
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
-class Home extends React.Component {
-    
- 
-  render() {
-   
-    return (
-     
-      <div className="container p-5">
-           <FullCalendar
-                    plugins={[dayGridPlugin, interactionPlugin]}
-                    droppable={true}
-                    editable={true}
-                    selectable={true}
-                    initialView='dayGridMonth'
-                    customButtons={{
-                        myCustomButton: {
-                          text: 'Custom Button!',
-                          click: function() {
-                            alert('clicked the custom button!');
-                          },
-                        },
-                      }}
-                    headerToolbar={{
-                      start: 'title', // will normally be on the left. if RTL, will be on the right
-                      center: 'today myCustomButton',
-                      end: 'prev,next', // will normally be on the right. if RTL, will be on the left
-                    }}
-                   
-                    events={[
-                      { title: 'event 1', date: '2023-10-08' },
-                      { title: 'event 2', date: '2023-10-10' },
-                    ]}
-                  />
-      </div>
-)
-};
+import React from "react";
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import "../styles/Dashboard.css";
+
+function Dashboard() {
+  return (
+    <div>
+      <Fullcalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={"dayGridMonth"}
+        headerToolbar={{
+          start: "today prev,next", // will normally be on the left. if RTL, will be on the right
+          center: "title",
+          end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
+        }}
+        allDaySlot={false}
+        scrollTime={'09:00:00'}
+        contentHeight={455}
+      />
+    </div>
+  );
 }
-export default Home;
+
+export default Dashboard;
