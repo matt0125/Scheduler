48b8b60a733dee4c7d766b174b0c98f63d42c761
Matthew Daley <m.daley0125@gmail.com>
2023-11-07 14:11:59 -0500

    Try reference passing

--- API/models/Shift.js
+++ API/models/Shift.js
@@ -4,8 +4,14 @@ const Schema = mongoose.Schema;
 // Define the schema for the 'Shift' collection
 const shiftSchema = new mongoose.Schema({
   date: Date,
-  empId: Schema.Types.ObjectId,
-  templateId: Schema.Types.ObjectId,
+  empId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Employee'
+  },
+  templateId:  {
+    type: Schema.Types.ObjectId,
+    ref: 'ShiftTemplate'
+  },
 });
 
 // Create a model for the 'Shift' collection and export it
