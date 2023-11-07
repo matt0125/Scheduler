14c22362e4e8c42e6a7a829bfcc11605913d690e
Matthew Daley <m.daley0125@gmail.com>
2023-11-07 14:58:40 -0500

    Update ShiftController.js

--- API/controllers/ShiftController.js
+++ API/controllers/ShiftController.js
@@ -251,13 +251,7 @@ exports.getShiftByEmpIdAndDate = async (req, res) => {
         $gte: startDate,
         $lte: endDate,
       },
-    }).populate('templateId')
-  .populate({
-    path: 'templateId',
-    populate: {
-      path: 'positionId',
-    },
-  });
+    }).populate('templateId').populate('templateId.positionId');
 
     if (!shifts || shifts.length === 0) {
       return res.status(404).json({ message: 'No shifts found for the specified employee ID' });
