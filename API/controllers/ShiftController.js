9d376792f3e6184d5fdfa73d0f607f79c32cb69b
Matthew Daley <122408204+matt0125@users.noreply.github.com>
2023-11-08 15:18:00 -0500

    Update ShiftController.js

--- API/controllers/ShiftController.js
+++ API/controllers/ShiftController.js
@@ -286,7 +286,7 @@ exports.getShiftByManager = async (req, res) => {
       return res.status(404).json({ message: 'No shifts found for managers' });
     }
 
-    res.status(200).json(shifts);
+    res.status(200).json({shifts: shifts});
   } 
   
   catch (error) {
