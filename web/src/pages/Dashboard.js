c94ec482e425aa997885c8c55098315767cd3fd6
Firebro51 <140781290+Firebro51@users.noreply.github.com>
2023-11-11 20:49:10 -0500

    More pages added
    Added a UpdateAvailabilty.js, and navigation between all the different pages

--- web/src/pages/Dashboard.js
+++ web/src/pages/Dashboard.js
@@ -96,14 +96,23 @@ export default class DemoApp extends React.Component {
         }
       });
   
-      const formattedShiftTemplates = formatShiftTemplatesForCalendar(response.data);
-      this.setState({ shiftTemplates: formattedShiftTemplates });
-      console.log(formattedShiftTemplates);
+      // Check if the response is an array
+      if (Array.isArray(response.data)) {
+        const formattedShiftTemplates = formatShiftTemplatesForCalendar(response.data);
+        this.setState({ shiftTemplates: formattedShiftTemplates });
+      } else {
+        // Handle case where response is not an array
+        console.error('Response is not an array', response.data);
+        console.log()
+        this.setState({ shiftTemplates: [] });
+      }
     } catch (error) {
       alert('Failed to fetch shift templates: ' + error.message);
       console.log(error);
+      this.setState({ shiftTemplates: [] }); // Reset to empty array on error
     }
   }
+  
 
   handlePositionSelect = (event) => {
     this.setState({ selectedPositionId: event.target.value });
