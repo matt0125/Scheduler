b2bfb9adb69d9656e17198f1146d91128676addf
Matthew Daley <m.daley0125@gmail.com>
2023-11-12 15:26:32 -0500

    Update EmployeeController.js

--- API/controllers/EmployeeController.js
+++ API/controllers/EmployeeController.js
@@ -232,7 +232,7 @@ exports.getTeammates = async (req, res) => {
       return res.status(404).json({ message: 'Employee not found' });
     }
 
-    const teammates = await Employee.find({managedBy: employee.managedBy, _id: { $ne: employeeId}}).select('-_id firstName lastName email phone positions').populate({path:'positions', select:'-id name'});
+    const teammates = await Employee.find({managedBy: employee.managedBy, _id: { $ne: employeeId}}).select('-_id firstName lastName email phone positions').populate({path:'positions', select:'-_id name'});
 
     const manager = await Employee.findById(employeeId).select('-_id managedBy').populate({path:'managedBy', select: '-_id firstName lastName email phone'});
 
