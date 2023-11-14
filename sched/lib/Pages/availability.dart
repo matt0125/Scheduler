4441ea04a0a19013d1f3a307208b277c441d45fe
JoshBell43 <bellfamily401@gmail.com>
2023-11-14 13:47:22 -0500

    Changed availability page
    Still a work in progress

--- sched/lib/Pages/availability.dart
+++ sched/lib/Pages/availability.dart
@@ -1,178 +1,204 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class AvailabilityScreen extends StatelessWidget {
+class AvailabilityScreen extends StatefulWidget {
+  @override
+  _AvailabilityScreenState createState() => _AvailabilityScreenState();
+}
+
+class _AvailabilityScreenState extends State<AvailabilityScreen> {
+  List<String> daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  List<List<bool>> availability = List.generate(7, (index) => List.filled(24, false));
+  List<List<String>> selectedTimes = List.generate(7, (index) => []);
+
+  int selectedDayIndex = -1;
+
+  @override
+  void initState() {
+    super.initState();
+    // Load saved availability from SharedPreferences
+    loadAvailability();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Select your availability',
-          style: TextStyle(
-            color: Color(0xFF49423E),
-          ),
-        ),
+        title: Text('Select your availability', style: TextStyle(color: Color(0xFF49423E))),
       ),
-      body: Row(
+      body: Column(
         children: [
-          // Days of the week column
-          Container(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              children: [
-                Text(
-                  'Sunday',
-                  style: TextStyle(
-                    fontFamily: 'Katibeh',
-                    fontSize: 35,
-                    color: Color(0xFF49423E),
+          // Days of the week row
+          AnimatedContainer(
+            duration: Duration(milliseconds: 500),
+            height: selectedDayIndex != -1 ? 50 : 150, // Adjusted height for animation
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: daysOfWeek
+                  .asMap()
+                  .entries
+                  .map((entry) => GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedDayIndex = selectedDayIndex == entry.key ? -1 : entry.key;
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: selectedDayIndex == entry.key ? Color(0xFFB1947B) : Colors.white,
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(10),
                   ),
+                  child: Text(entry.value, style: TextStyle(fontFamily: 'Katibeh', fontSize: 20, color: Colors.black)),
                 ),
-                SizedBox(height: 15), // Add space between Sunday and Monday
-                Text(
-                  'Monday',
-                  style: TextStyle(
-                    fontFamily: 'Katibeh',
-                    fontSize: 35,
-                    color: Color(0xFF49423E),
-                  ),
-                ),
-                SizedBox(height: 15), // Add space between Monday and Tuesday
-                Text(
-                  'Tuesday',
-                  style: TextStyle(
-                    fontFamily: 'Katibeh',
-                    fontSize: 35,
-                    color: Color(0xFF49423E),
-                  ),
-                ),
-                SizedBox(height: 15), // Add space between Tuesday and Wednesday
-                Text(
-                  'Wednesday',
-                  style: TextStyle(
-                    fontFamily: 'Katibeh',
-                    fontSize: 35,
-                    color: Color(0xFF49423E),
-                  ),
-                ),
-                SizedBox(height: 15), // Add space between Wednesday and Thursday
-                Text(
-                  'Thursday',
-                  style: TextStyle(
-                    fontFamily: 'Katibeh',
-                    fontSize: 35,
-                    color: Color(0xFF49423E),
-                  ),
-                ),
-                SizedBox(height: 15), // Add space between Thursday and Friday
-                Text(
-                  'Friday',
-                  style: TextStyle(
-                    fontFamily: 'Katibeh',
-                    fontSize: 35,
-                    color: Color(0xFF49423E),
-                  ),
-                ),
-                SizedBox(height: 15), // Add space between Friday and Saturday
-                Text(
-                  'Saturday',
-                  style: TextStyle(
-                    fontFamily: 'Katibeh',
-                    fontSize: 35,
-                    color: Color(0xFF49423E),
-                  ),
-                ),
-              ],
+              ))
+                  .toList(),
             ),
           ),
-          // Drop-down boxes
-          Container(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              children: [
-                DropdownButton<String>(
-                  items: ['Option 1', 'Option 2', 'Option 3']
-                      .map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
+          // Selected day hours
+          if (selectedDayIndex != -1)
+            Expanded(
+              child: SingleChildScrollView(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+                  itemCount: 24,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          availability[selectedDayIndex][index] = !availability[selectedDayIndex][index];
+                          if (availability[selectedDayIndex][index]) {
+                            updateSelectedTimes(selectedDayIndex, index, true);
+                          } else {
+                            updateSelectedTimes(selectedDayIndex, index, false);
+                          }
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            color: availability[selectedDayIndex][index] ? Color(0xFFCDBFB6) : Colors.white),
+                        child: Center(
+                          child: Text(
+                            '${index % 12 == 0 ? 12 : index % 12}:00 ${index < 12 ? 'AM' : 'PM'}',
+                            style: TextStyle(fontSize: 14, color: Colors.black),
+                          ),
+                        ),
+                      ),
                     );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    // Handle the drop-down value change
                   },
                 ),
-                SizedBox(height: 15), // Add space between drop-down boxes
-                DropdownButton<String>(
-                  items: ['Option A', 'Option B', 'Option C']
-                      .map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    // Handle the drop-down value change
-                  },
-                ),
-                SizedBox(height: 15),
-                DropdownButton<String>(
-                  items: ['Option A', 'Option B', 'Option C']
-                      .map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    // Handle the drop-down value change
-                  },
-                ),
-                SizedBox(height: 15),
-                DropdownButton<String>(
-                  items: ['Option A', 'Option B', 'Option C']
-                      .map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    // Handle the drop-down value change
-                  },
-                ),
-                SizedBox(height: 15),
-                DropdownButton<String>(
-                  items: ['Option A', 'Option B', 'Option C']
-                      .map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    // Handle the drop-down value change
-                  },
-                ),
-                SizedBox(height: 15),
-                DropdownButton<String>(
-                  items: ['Option A', 'Option B', 'Option C']
-                      .map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    // Handle the drop-down value change
-                  },
-                ),
-                // Add space between drop-down boxes
-                // Repeat for other days of the week
-              ],
+              ),
             ),
+          // Selected times for each day
+          Expanded(
+            child: ListView.builder(
+              itemCount: daysOfWeek.length,
+              itemBuilder: (context, dayIndex) {
+                return ListTile(
+                  title: Text(
+                    '${daysOfWeek[dayIndex]}: ${formatSelectedTimes(selectedTimes[dayIndex])}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                );
+              },
+            ),
+          ),
+          // Save and Clear All Availability buttons
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  saveAvailability();
+                },
+                child: Text('Save'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  clearAllAvailability();
+                },
+                child: Text('Clear All Availability'),
+              ),
+            ],
           ),
         ],
       ),
     );
   }
+
+  void updateSelectedTimes(int dayIndex, int timeIndex, bool isAdding) {
+    if (isAdding) {
+      selectedTimes[dayIndex].add('${timeIndex % 12 == 0 ? 12 : timeIndex % 12}:00 ${timeIndex < 12 ? 'AM' : 'PM'}');
+    } else {
+      selectedTimes[dayIndex].remove('${timeIndex % 12 == 0 ? 12 : timeIndex % 12}:00 ${timeIndex < 12 ? 'AM' : 'PM'}');
+    }
+    selectedTimes[dayIndex].sort((a, b) => int.parse(a.split(':')[0]).compareTo(int.parse(b.split(':')[0])));
+  }
+
+  String formatSelectedTimes(List<String> times) {
+    if (times.isEmpty) return 'No selected times';
+    List<String> formattedTimes = [];
+    int start = 0, end = 0;
+    for (int i = 1; i < times.length; i++) {
+      if (int.parse(times[i - 1].split(':')[0]) + 1 == int.parse(times[i].split(':')[0]) &&
+          times[i - 1].split(' ')[1] == times[i].split(' ')[1]) {
+        end = i;
+      } else {
+        if (start == end) {
+          formattedTimes.add(times[start]);
+        } else {
+          formattedTimes.add('${times[start]}-${times[end]}');
+        }
+        start = end = i;
+      }
+    }
+    if (start == end) {
+      formattedTimes.add(times[start]);
+    } else {
+      formattedTimes.add('${times[start]}-${times[end]}');
+    }
+    return formattedTimes.join(', ');
+  }
+
+  // Save availability to SharedPreferences
+  Future<void> saveAvailability() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    for (int i = 0; i < daysOfWeek.length; i++) {
+      prefs.setStringList('$i', selectedTimes[i]);
+    }
+  }
+
+  // Load availability from SharedPreferences
+  Future<void> loadAvailability() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    for (int i = 0; i < daysOfWeek.length; i++) {
+      List<String>? savedTimes = prefs.getStringList('$i');
+      if (savedTimes != null) {
+        selectedTimes[i] = savedTimes;
+        for (String time in savedTimes) {
+          int timeIndex = int.parse(time.split(':')[0]);
+          availability[i][timeIndex] = true;
+        }
+      }
+    }
+    setState(() {}); // Force a rebuild of the widget
+  }
+
+  // Clear all selected times
+  void clearAllAvailability() {
+    for (int i = 0; i < daysOfWeek.length; i++) {
+      selectedTimes[i].clear();
+      for (int j = 0; j < 24; j++) {
+        availability[i][j] = false;
+      }
+    }
+    setState(() {});
+  }
+}
+
+void main() {
+  runApp(MaterialApp(home: AvailabilityScreen()));
 }
