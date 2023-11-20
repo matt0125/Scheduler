7777a52adbb1c40f08c9124c22b7b96c58efe2a5
Julianne Tomlinson <118313562+JewlzT@users.noreply.github.com>
2023-11-20 13:34:52 -0500

    Basic Validation for Register Page

--- web/src/pages/Register.js
+++ web/src/pages/Register.js
@@ -27,11 +27,45 @@ const Register = () => {
     managerIdent: false
   });
 
+  const [formErrors, setFormErrors] = useState({
+    username: '',
+    password: '',
+    email: '',
+    firstName: '',
+    lastName: '',
+    phone: '',
+  });
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value
+    });
+
+    validateField(name, value);
+  };
+
+  const validateField = (name, value) => {
+    let error = '';
+
+    if (name === 'username') {
+      if (value.length < 2) {
+        error = 'Username must be at least 2 characters long.';
+      }
+    } 
+    if (name === 'email') {
+      // Example: Check if the email is a valid email address
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(value)) {
+        error = 'Enter a valid email address.';
+      }
+    }
+
+    // Update the form errors
+    setFormErrors({
+      ...formErrors,
+      [name]: error,
     });
   };
 
@@ -46,6 +80,7 @@ const Register = () => {
     }
   };
 
+ 
 
   let url = "/";
   return (
@@ -64,36 +99,38 @@ const Register = () => {
             <img src={vector} className="business-photo" alt="business vector"/>
           </Col>
           <Col className="main-column">            
-            <form onSubmit={handleSubmit}>
+            <form id ="myForm" onSubmit={handleSubmit}>
             <div className='register-form'>
             <h1 class="font-family-katibeh">Register</h1>
             <div className='register-box'>
               <div class="firstName-input-group">  
               <h2 class="input-font">First Name</h2>
-                <input type="text" name="firstName" placeholder="" onChange={handleChange} />
+                <input type="text" name="firstName" placeholder="" required onChange={handleChange} />
               </div>
               <div class="lastName-input-group"> 
               <h2 class="input-font">Last Name</h2>
-                <input type="text" name="lastName" placeholder="" onChange={handleChange} />
+                <input type="text" name="lastName" placeholder="" required onChange={handleChange} />
               </div>
               <div class="user-input-group"> 
               <h2 class="input-font">Username</h2>
-                <input type="text" name="username" placeholder="" onChange={handleChange} />
+                <input type="text" name="username" placeholder="" required onChange={handleChange} />
+                {formErrors.username && <div className="user-popup">{formErrors.username}</div>}
               </div>
               <div class="pass-input-group"> 
               <h2 class="input-font">Password</h2>
               <img src={passIcon} alt="pass icon" /> 
-                <input type="password" name="password" placeholder="" onChange={handleChange} />
+                <input type="password" name="password" placeholder="" required onChange={handleChange} />
               </div>
               <div class="email-input-group">
               <h2 class="input-font">Email</h2>
               <img src={emailIcon} alt="email icon" /> 
-                <input type="email" name="email" placeholder="" onChange={handleChange} />
+                <input type="email" name="email" placeholder="" required onChange={handleChange} />
+                {formErrors.email && <div className="popup">{formErrors.email}</div>}
               </div>
               <div class="phone-input-group">
               <h2 class="input-font">Phone Number</h2>
               <img src={phoneIcon} alt="email icon" /> 
-                <input type="text" name="phone" placeholder="" onChange={handleChange} />
+                <input type="text" name="phone" placeholder="" required onChange={handleChange} />
               </div>
             </div>
             <div class="managerID-input-group">
@@ -104,6 +141,7 @@ const Register = () => {
                 value="Manager" 
                 id="manager"
                 name="title"
+                required
                 />
                 <label for="manager" onClick={(e) => handleChange({ target: { name: 'managerIdent', value: true } })}>
                   Manager
