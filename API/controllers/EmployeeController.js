cf2455529c7c685f9a6b80d8856c2529e7461d64
Evan Naderi <evan.naderi@gmail.com>
2023-11-20 16:31:20 -0500

    password complexity

--- API/controllers/EmployeeController.js
+++ API/controllers/EmployeeController.js
@@ -52,6 +52,27 @@ exports.registerEmployee = async (req, res) => {
       return res.status(400).json({ message: 'Username must be between 2 to 20 characters' });
     }
 
+    // Password complexity validations
+    if (password.length < 8) {
+      return res.status(400).json({ message: 'Password must be at least 8 characters long' });
+    }
+
+    if (!/[A-Z]/.test(password)) {
+      return res.status(400).json({ message: 'Password must contain at least one uppercase letter' });
+    }
+
+    if (!/[a-z]/.test(password)) {
+      return res.status(400).json({ message: 'Password must contain at least one lowercase letter' });
+    }
+
+    if (!/[0-9]/.test(password)) {
+      return res.status(400).json({ message: 'Password must contain at least one number' });
+    }
+
+    if (!/[^A-Za-z0-9]/.test(password)) {
+      return res.status(400).json({ message: 'Password must contain at least one special character' });
+    }
+
     // Hash password before storing it
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
