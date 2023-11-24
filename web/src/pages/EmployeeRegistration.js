4e10f35ab174a2fdc5bbe1e8f484831960a2ca10
Firebro51 <140781290+Firebro51@users.noreply.github.com>
2023-11-23 23:21:29 -0500

    Manager search and find in employeRegistartion
    Manager search and find in employeRegistartion

--- web/src/pages/EmployeeRegistration.js
+++ web/src/pages/EmployeeRegistration.js
@@ -1,26 +1,43 @@
-import React, { useState } from 'react';
-import { Container, TextField, Button, Typography, Box, MenuItem, Select, InputLabel, FormControl } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container, Button, Typography, Box, TextField, FormControl } from '@mui/material';
+import Autocomplete from '@mui/lab/Autocomplete';
 
 const EmployeeRegistration = () => {
   const [manager, setManager] = useState('');
+  const [managers, setManagers] = useState([]);
+  const jwtToken = localStorage.getItem('token'); 
 
-  const handleManagerChange = (event) => {
-    setManager(event.target.value);
+  useEffect(() => {
+    const fetchManagers = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/managers', {
+          headers: {
+            'Authorization': `Bearer ${jwtToken}`,
+            'Content-Type': 'application/json'
+          }
+        });
+
+        // Assuming the response data is in the format { managers: [...] }
+        const managerList = response.data.managers.filter(user => user.managerIdent);
+
+        setManagers(managerList);
+      } catch (error) {
+        console.error('Error fetching managers:', error);
+      }
+    };
+
+    fetchManagers();
+  }, []);
+
+  const handleManagerChange = (event, value) => {
+    setManager(value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Handle the form submission, e.g., send data to a backend server
     console.log(`Selected Manager: ${manager}`);
-    // Assuming you'd want to do something with the selected manager here
   };
-
-  // Dummy list of managers for the dropdown
-  const managers = [
-    { name: 'Manager 1', id: 'manager1' },
-    { name: 'Manager 2', id: 'manager2' },
-    // ... other managers
-  ];
 
   return (
     <Container maxWidth="sm">
@@ -30,20 +47,19 @@ const EmployeeRegistration = () => {
         </Typography>
         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
           <FormControl fullWidth margin="normal">
-            <InputLabel id="manager-select-label">Whose your manager?</InputLabel>
-            <Select
-              labelId="manager-select-label"
-              id="manager"
-              value={manager}
-              label="Whose your manager?"
+            <Autocomplete
+              id="manager-autocomplete"
+              options={managers}
+              getOptionLabel={(option) => option.username}
               onChange={handleManagerChange}
-            >
-              {managers.map((mgr) => (
-                <MenuItem key={mgr.id} value={mgr.name}>
-                  {mgr.name}
-                </MenuItem>
-              ))}
-            </Select>
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Who's your manager (by username)?"
+                  variant="outlined"
+                />
+              )}
+            />
           </FormControl>
           <Button
             type="submit"
