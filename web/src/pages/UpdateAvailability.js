0ca27f40bf1c7e6bbfc00abd78a6eb8c2c310775
eddymarg <103199715+eddymarg@users.noreply.github.com>
2023-11-25 00:48:03 -0500

    tried to make API work

--- web/src/pages/UpdateAvailability.js
+++ web/src/pages/UpdateAvailability.js
@@ -65,26 +65,26 @@ const UpdateAvailability = () => {
   };
 
   // Function to handle manager name and position change
-  const handleSelectChange = (e) => {
-    setAvailability((prevAvailability) => ({
-      ...prevAvailability,
-      [e.target.name]: e.target.value,
-    }));
-  };
+  // const handleSelectChange = (e) => {
+  //   setAvailability((prevAvailability) => ({
+  //     ...prevAvailability,
+  //     [e.target.name]: e.target.value,
+  //   }));
+  // };
 
   // Function to submit the form
   const handleSubmit = async (e) => {
     e.preventDefault();
     // Construct the payload, you might need to adjust this based on your API requirements
     const payload = {
-      managerName: availability.managerName,
-      position: availability.position,
-      days: availability.days,
+      dayOfWeek: availability.managerName,
+      startTime: availability.position,
+      endTime: availability.days,
     };
 
     try {
       // Replace 'YOUR_API_ENDPOINT' with the actual endpoint of your backend
-      const response = await axios.post('http://large.poosd-project.com/api/employee/654c4136aa30871b5bd30826/availability/654c7e645ef9e3156f0fd1cd', payload);
+      const response = await axios.put('http://large.poosd-project.com/api/employee/654c4136aa30871b5bd30826/availability/654c7e645ef9e3156f0fd1cd', payload);
 
       // Handle the response as needed
       console.log(response.data);
@@ -112,18 +112,19 @@ const UpdateAvailability = () => {
               {/* Availability for each day */}
               <div className="availabilities">
               {Object.keys(availability.days).map((day) => (
-                <div key={day} className="d-flex align-items-center">
-                  <label>
-                    <input
-                      type="checkbox"
-                      checked={availability.days[day].available}
-                      onChange={() => handleCheckboxChange(day)}
-                    />
-                    {day}:
-                  </label>
-                  <div className="time-selects"> 
-                    <Col className="start"> 
-                      <select
+                <Row key={day} className="d-flex align-items-center">
+                  <Col>
+                    <label>
+                      <input
+                        type="checkbox"
+                        checked={availability.days[day].available}
+                        onChange={() => handleCheckboxChange(day)}
+                      />
+                      {day}:
+                    </label>
+                  </Col>
+                  <Col className="time-selects d-flex align-items-start"> 
+                      <select className="start mr-2"
                         value={availability.days[day].startTime}
                         onChange={(e) => handleTimeChange(day, true, e.target.value)}
                       >
@@ -131,9 +132,7 @@ const UpdateAvailability = () => {
                           <option key={`${day}-start-${time}`} value={time}>{time}</option>
                         ))}
                       </select>
-                    </Col>
-                    <Col className="end">
-                      <select
+                      <select className="end mr-2"
                         value={availability.days[day].endTime}
                         onChange={(e) => handleTimeChange(day, false, e.target.value)}
                       >
@@ -141,12 +140,11 @@ const UpdateAvailability = () => {
                           <option key={`${day}-end-${time}`} value={time}>{time}</option>
                       ))}
                       </select>
-                    </Col>
-                  </div>
-                </div>
+                  </Col>
+                </Row>
               ))}
               </div>
-              <Row>
+              <Row className="mt-3">
                   <button type="button" className="button-container" onClick={() => window.history.back()}>Back</button>
                   <button type="submit" className="button-container">Update Availability</button>
               </Row>
