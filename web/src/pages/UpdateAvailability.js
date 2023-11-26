307d1df6acccaf71cb0c415da2970e1611da0cea
eddymarg <103199715+eddymarg@users.noreply.github.com>
2023-11-26 01:08:51 -0500

    trying to fix API component

--- web/src/pages/UpdateAvailability.js
+++ web/src/pages/UpdateAvailability.js
@@ -2,21 +2,20 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import "../styles/UpdateAvailability.css";
 import logo from "../images/branding-notitle.png";
-import {Container, Row, Col } from "react-bootstrap";
-
+import { Container, Row, Col } from "react-bootstrap";
 
 const generateTimeSlots = (interval = 30) => {
-    const times = [];
-    let currentTime = new Date().setHours(0, 0, 0, 0); // Start at midnight
-  
-    while (currentTime <= new Date().setHours(23, 59, 0, 0)) {
-      let time = new Date(currentTime);
-      times.push(time.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true }));
-      currentTime += interval * 60 * 1000; // Add interval minutes
-    }
-  
-    return times;
-  };
+  const times = [];
+  let currentTime = new Date().setHours(0, 0, 0, 0); // Start at midnight
+
+  while (currentTime <= new Date().setHours(23, 59, 0, 0)) {
+    let time = new Date(currentTime);
+    times.push(time.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true }));
+    currentTime += interval * 60 * 1000; // Add interval minutes
+  }
+
+  return times;
+};
 
 // Default interval of 30 minutes
 const timeOptions = generateTimeSlots();
@@ -64,23 +63,21 @@ const UpdateAvailability = () => {
     }));
   };
 
-  // Function to handle manager name and position change
-  // const handleSelectChange = (e) => {
-  //   setAvailability((prevAvailability) => ({
-  //     ...prevAvailability,
-  //     [e.target.name]: e.target.value,
-  //   }));
-  // };
-
   // Function to submit the form
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Construct the payload, you might need to adjust this based on your API requirements
+    
     const payload = {
-      dayOfWeek: availability.managerName,
-      startTime: availability.position,
-      endTime: availability.days,
-    };
+      availability: [],
+    }
+
+    Object.keys(availability.days).forEach((day) => {
+      payload.availability.push({
+        dayOfWeek: day,
+        startTime: availability.days[day].startTime,
+        endTime: availability.days[day].endTime,
+      });
+    });
 
     try {
       // Replace 'YOUR_API_ENDPOINT' with the actual endpoint of your backend
@@ -90,7 +87,13 @@ const UpdateAvailability = () => {
       console.log(response.data);
       alert('Availability updated successfully!');
     } catch (error) {
-      console.error('There was an error updating the availability:', error);
+      if (error.isAxiosError) {
+        console.error('Axios Error:', error.message);
+        console.error('Axios Response:', error.response);
+      } else {
+        // Other types of errors
+        console.error('Error updating availability:', error);
+      }
 
       alert('Failed to update availability.');
     }
@@ -99,18 +102,18 @@ const UpdateAvailability = () => {
   return (
     <Container className="update-Avail-Page">
       <Row>
-        <Col xs={12} md={3}>
+        <Col xs={12} md={2}>
           <div className="logo-container">
             <h1>Sched</h1>
             <img src={logo} alt="sched logo" className="logo"></img>
           </div>
         </Col>
-        <Col xs={12} md={{span: 6, offset: 3}} className="background-square">
-            <h2 className="update-title">Update Availability</h2>
-            <form onSubmit={handleSubmit}>
-              <h3>What days are you available?</h3>
-              {/* Availability for each day */}
-              <div className="availabilities">
+        <Col xs={12} md={{ span: 6, offset: 2 }} className="background-square">
+          <h2 className="update-title">Update Availability</h2>
+          <form onSubmit={handleSubmit}>
+            <h3>What days are you available?</h3>
+            {/* Availability for each day */}
+            <div className="availabilities">
               {Object.keys(availability.days).map((day) => (
                 <Row key={day} className="d-flex align-items-center">
                   <Col>
@@ -123,34 +126,37 @@ const UpdateAvailability = () => {
                       {day}:
                     </label>
                   </Col>
-                  <Col className="time-selects d-flex flex-column align-items-start"> 
-                      <select className="start mb-2"
-                        value={availability.days[day].startTime}
-                        onChange={(e) => handleTimeChange(day, true, e.target.value)}
-                      >
-                        {timeOptions.map(time => (
-                          <option key={`${day}-start-${time}`} value={time}>{time}</option>
-                        ))}
-                      </select>
-                      <select className="end mb-2"
-                        value={availability.days[day].endTime}
-                        onChange={(e) => handleTimeChange(day, false, e.target.value)}
-                      >
+                  {/* Separate columns for start and end select dropdowns */}
+                  <Col className="start-column d-flex flex-column align-items-start">
+                    <select className="start mb-2"
+                      value={availability.days[day].startTime}
+                      onChange={(e) => handleTimeChange(day, true, e.target.value)}
+                    >
                       {timeOptions.map(time => (
-                          <option key={`${day}-end-${time}`} value={time}>{time}</option>
+                        <option key={`${day}-start-${time}`} value={time}>{time}</option>
                       ))}
-                      </select>
+                    </select>
+                  </Col>
+                  <Col className="end-column d-flex flex-column align-items-start">
+                    <select className="end mb-2"
+                      value={availability.days[day].endTime}
+                      onChange={(e) => handleTimeChange(day, false, e.target.value)}
+                    >
+                      {timeOptions.map(time => (
+                        <option key={`${day}-end-${time}`} value={time}>{time}</option>
+                      ))}
+                    </select>
                   </Col>
                 </Row>
               ))}
-              </div>
-              <Row className="mt-3">
-                  <button type="button" className="button-container" onClick={() => window.history.back()}>Back</button>
-                  <button type="submit" className="button-container">Update Availability</button>
-              </Row>
-              </form>
-          </Col>
-        </Row>
+            </div>
+            <Row className="mt-3">
+              <button type="button" className="button-container" onClick={() => window.history.back()}>Back</button>
+              <button type="submit" className="button-container">Update Availability</button>
+            </Row>
+          </form>
+        </Col>
+      </Row>
     </Container>
   );
 };
