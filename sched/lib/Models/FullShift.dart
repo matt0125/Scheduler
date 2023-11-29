0ba997450ee80ec3b64419bdc98b18b4dce3463e
JoshBell43 <bellfamily401@gmail.com>
2023-11-29 11:21:33 -0500

    Daily schedule is in time order now

--- sched/lib/Models/FullShift.dart
+++ sched/lib/Models/FullShift.dart
@@ -7,6 +7,7 @@ class FullShift {
   String? fullName;
   String? printTime;
   String? printPositionTitle;
+  DateTime? shiftStartTime; // Add this property
 
   FullShift({
     this.startTime,
@@ -14,40 +15,40 @@ class FullShift {
     this.positionTitle,
     this.firstName,
     this.lastName,
-  }){
+  }) {
     this.fullName = "${this.firstName} ${this.lastName}";
-    if(this.startTime != null && this.endTime != null)
-    {
+    if (this.startTime != null && this.endTime != null) {
       int startH = int.parse(this.startTime!.split(":")[0]);
-      int endH = int.parse(this.endTime!.split(":")[0]);
       int startM = int.parse(this.startTime!.split(":")[1]);
+
+      // Create DateTime object for shift start time
+      this.shiftStartTime = DateTime(2023, 1, 1, startH, startM);
+
+      int endH = int.parse(this.endTime!.split(":")[0]);
       int endM = int.parse(this.endTime!.split(":")[1]);
 
       String startAM = startH < 12 ? "AM" : "PM";
       String endAM = endH < 12 ? "AM" : "PM";
 
-      if(startH > 12)
-        startH -= 12;
-      if(endH > 12)
-        endH -= 12;
+      if (startH > 12) startH -= 12;
+      if (endH > 12) endH -= 12;
 
-      this.printTime = "${startH}:${startM.toString().padLeft(2, '0')} ${startAM != endAM ? startAM : ""}- ${endH}:${endM.toString().padLeft(2, '0')} ${endAM}";
+      this.printTime =
+      "${startH}:${startM.toString().padLeft(2, '0')} ${startAM != endAM ? startAM : ""}- ${endH}:${endM.toString().padLeft(2, '0')} ${endAM}";
 
       this.printPositionTitle = format(this.positionTitle);
     }
   }
 
   String? format(String? positionTitle) {
-    if(positionTitle != null)
-      {
-        List<String> ogWords = positionTitle.split(" ");
-        List<String> newWords = [];
-        for(String word in ogWords)
-          {
-            newWords.add(word[0].toUpperCase() + word.substring(1).toLowerCase());
-          }
-        return newWords.join(" ");
+    if (positionTitle != null) {
+      List<String> ogWords = positionTitle.split(" ");
+      List<String> newWords = [];
+      for (String word in ogWords) {
+        newWords.add(word[0].toUpperCase() + word.substring(1).toLowerCase());
       }
+      return newWords.join(" ");
+    }
     return null;
   }
 }
