a8d4c468317c28f64c9cb3f18d50b3a6d25d6d61
Julianne Tomlinson <118313562+JewlzT@users.noreply.github.com>
2023-11-29 02:05:51 -0500

    Reset password page styling

--- web/src/pages/ResetPasswordPage.js
+++ web/src/pages/ResetPasswordPage.js
@@ -1,16 +1,33 @@
 import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import '../styles/Login.css'
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../images/branding-notitle.png";
+import vector from "../images/table-meeting.png";
+import eyeOpenSvg from '../images/eye-open-svg.svg';
+import eyeClosedSvg from '../images/eye-closed-svg.svg';
 
 const ResetPasswordPage = () => {
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
   const [success, setSuccess] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword2, setShowPassword2] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
 
   const { resetToken } = useParams();
   const navigate = useNavigate(); // Corrected variable name
   console.log("Reset token:", resetToken);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(prev => !prev);
+  };
+
+  const togglePasswordVisibility2 = () => {
+    setShowPassword2(prev => !prev);
+  };
 
   const handleResetPassword = async () => {
     if (newPassword !== confirmPassword) {
@@ -31,24 +48,106 @@ const ResetPasswordPage = () => {
     }
   };
 
+  const [formErrors, setFormErrors] = useState({
+    username: '',
+    password: '',
+    email: '',
+    firstName: '',
+    lastName: '',
+    phone: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    validateField(name, value);
+  };
+
+  const validateField = (name, value) => {
+    let error = '';
+    if (name === 'password') {
+      if (value.length < 8) {
+        error = 'Password must be at least 8 characters long.';
+      }
+      if (!/[A-Z]/.test(value)) {
+        error = 'Password must contain at least one uppercase letter';
+      }
+      if (!/[a-z]/.test(value)) {
+        error = 'Password must contain at least one lowercase letter';
+      }
+      if (!/[0-9]/.test(value)) {
+        error = 'Password must contain at least one number';
+      }
+      if (!/[^A-Za-z0-9]/.test(value)) {
+        error = 'Password must contain at least one special character';
+      }
+    } 
+
+    setFormErrors({
+      ...formErrors,
+      [name]: error,
+    });
+  };
+
+
   return (
-    <div>
-      <h2>Reset Password</h2>
-      {error && <div style={{ color: 'red' }}>{error}</div>}
-      {success && <div style={{ color: 'green' }}>{success}</div>}
-      <input
-        type="password"
-        placeholder="New Password"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Confirm New Password"
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-      />
-      <button onClick={handleResetPassword}>Reset Password</button>
+  <div id="login-body">
+    <Container>
+      <Row>
+        <Col>
+          <img src={logo} alt="sched logo" className="logo"></img>
+        </Col>
+        <Col className="title">
+          <h1>Sched</h1>
+        </Col>
+      </Row>
+      <Row className="row"> 
+        <Col className="column">
+          <img src={vector} id='reset-photo' className="business-photo" alt="business vector"/>
+        </Col>
+        <Col className="main-column">
+        {error && <div style={{ color: 'red' , alignItems: 'center'}}>{error}</div>}
+        {success && <div style={{ color: 'green' }}>{success}</div>}
+        <div className="login-box">
+        <h2 class="font-family-katibeh">Reset Password</h2>
+        <div class="new-pass">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="New Password"
+            value={newPassword}
+            name="password"
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
+          <div className="checkbox_wrapper" onClick={togglePasswordVisibility}>
+                  <img
+                    src={showPassword ? eyeOpenSvg : eyeClosedSvg}
+                    alt="Toggle Password Visibility"
+                    className="eye-icon"
+                  />
+          </div>
+          {formErrors.password && <div className="pass-popup">{formErrors.password}</div>}
+          </div>
+
+          <div class="confirm-pass">
+          <input
+            type={showPassword2 ? "text" : "password"}
+            placeholder="Confirm New Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          </div>
+          <div className="checkbox_wrapper2" onClick={togglePasswordVisibility2}>
+                  <img
+                    src={showPassword2 ? eyeOpenSvg : eyeClosedSvg}
+                    alt="Toggle Password Visibility"
+                    className="eye-icon"
+                  />
+          </div>
+        <button className="submit-button" onClick={handleResetPassword}>Reset Password</button>
+        </div>
+        </Col>
+      </Row>
+    </Container>
     </div>
   );
 };
